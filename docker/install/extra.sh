c546cacac39fd3f727e806038062775e9b8a1c61
James Goppert <james.goppert@gmail.com>
2023-01-28 17:39:56 -0500

    Add docker.
    Signed-off-by: James Goppert <james.goppert@gmail.com>

---
+++ docker/install/extra.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo DEBIAN_FRONTEND=noninteractive  apt-get install --no-install-recommends -y \
+	htop \
+	iproute2 \
+	lcov \
+	gosu \
+	menu \
+	mesa-utils \
+	novnc \
+	libturbojpeg \
+	openbox \
+	python3-jinja2 \
+	python3-numpy \
+	python3-websockify \
+	python3-xdg \
+	python3-xmltodict \
+	qt5dxcb-plugin \
+	screen \
+	terminator \
+	vim \
+	xfce4
+
